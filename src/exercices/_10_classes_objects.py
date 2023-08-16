bef0eee861a454bb791dd40cdc14d7b49e404f9d
MenSeb <sebastien.menard.github@gmail.com>
2023-08-16 17:07:23 -0400

    chore(tutorials): 10 - classes and objects

---
+++ src/exercices/_10_classes_objects.py
@@ -0,0 +1,37 @@
+"""classes and objects."""
+
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+
+    def __init__(
+        self: "Vehicle",
+        name: str,
+        kind: str,
+        color: str,
+        value: float,
+    ) -> None:
+        self.name = name
+        self.kind = kind
+        self.color = color
+        self.value = value
+
+    def description(self: "Vehicle") -> str:
+        return f"{self.name} is a {self.color} {self.kind} worth ${self.value:.2f}."
+
+
+def main() -> None:
+    # your code goes here
+    car1 = Vehicle("Fer", "convertible", "red", 60000.00)
+    car2 = Vehicle("Jump", "van", "blue", 10000.00)
+
+    # test code
+    print(car1.description())
+    print(car2.description())
+
+
+main()
