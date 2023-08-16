01d7878bef6c39878bba060db5495933b9ccfc19
MenSeb <sebastien.menard.github@gmail.com>
2023-08-16 15:58:51 -0400

    chore(tutorials): 5 - string formatting

---
+++ src/exercices/_5_string_formatting.py
@@ -0,0 +1,12 @@
+"""String formatting."""
+
+
+def string_formatting() -> None:
+    """Print string formatting."""
+    data = ("John", "Doe", 53.44)
+    format_string = "Hello %s %s. Your current balance is $%.2f"
+
+    print(format_string % data)
+
+
+string_formatting()
