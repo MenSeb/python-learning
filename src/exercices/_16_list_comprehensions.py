f1c3856457d30a6ce5b0af9a4cc986029348553a
MenSeb <sebastien.menard.github@gmail.com>
2023-08-16 22:56:31 -0400

    chore(tutorials): 16 - list comprehensions

---
+++ src/exercices/_16_list_comprehensions.py
@@ -0,0 +1,42 @@
+"""list comprehensions."""
+
+from __future__ import annotations
+
+from typing import Callable, Sequence
+
+
+def filter_positive(
+    numbers: Sequence[int | float],
+    callback: Callable | None = None,
+) -> list[int | float]:
+    """Filter a list of numbers keeping only the positives."""
+    return [callback(n) if callback else n for n in numbers if n >= 0]
+
+
+def main() -> None:
+    """Print list comprehensions."""
+    numbers_float = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+    positive_numbers_float = filter_positive(numbers_float)
+
+    print("Positive numbers float")
+    print(positive_numbers_float, "\n")
+
+    numbers_int = [34, -203, 44, 68, -12, 44, 12]
+    positive_numbers_int = filter_positive(numbers_int)
+
+    print("Positive numbers int")
+    print(positive_numbers_int, "\n")
+
+    numbers_mix = [34.6, -203.4, 44.9, 68, -12, 44, 12.7]
+    positive_numbers_mix = filter_positive(numbers_mix)
+
+    print("Positive numbers mix")
+    print(positive_numbers_mix, "\n")
+
+    positive_numbers_converted = filter_positive(numbers_float, int)
+
+    print("Positive numbers converted to integers")
+    print(positive_numbers_converted, "\n")
+
+
+main()
