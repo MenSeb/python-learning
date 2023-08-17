2548f8144f2d1643fbb9e7b83b9dc2a4348b201d
MenSeb <sebastien.menard.github@gmail.com>
2023-08-17 11:02:39 -0400

    chore(tutorials): 23 - partial functions

---
+++ src/exercices/_23_partial_functions.py
@@ -0,0 +1,27 @@
+"""partial functions."""
+
+from functools import partial
+
+
+def multiply(x: int, y: int) -> int:
+    """Multiply x by y."""
+    return x * y
+
+
+def equation(u: int, v: int, w: int, x: int) -> int:
+    """Multiply u by 4, v by 3, w by 2 and add everything to x."""
+    return u * 4 + v * 3 + w * 2 + x
+
+
+def main() -> None:
+    """Print partial functions."""
+    double = partial(multiply, 2)
+
+    print(double(4))
+
+    partial_equation = partial(equation, 5, 10, 5)
+
+    print(partial_equation(0))
+
+
+main()
