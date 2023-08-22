f09ab75b51eabd88ac781b92db1c62b8831c181b
MenSeb <sebastien.menard.github@gmail.com>
2023-08-22 18:10:38 -0400

    chore(basics): exercise 2

---
+++ src/tutorials/pynative/_1_basics/exercise2.py
@@ -0,0 +1,14 @@
+"""Exercise 2: Print the sum of the current number and the previous number."""
+
+
+def main() -> None:
+    """Print current and previous number sum in a range."""
+    n = 10
+    n1 = 0
+    n2 = 0
+    for _number in range(n):
+        print("Current Number %d Previous Number %d Sum: %d" % (n2, n1, n1 + n2))
+        n1, n2 = n2, n2 + 1
+
+
+main()
