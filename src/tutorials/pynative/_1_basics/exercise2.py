24a000a624e11d5b3fb7febb22f24f419fe7d727
MenSeb <sebastien.menard.github@gmail.com>
2023-08-22 19:14:13 -0400

    chore(basics): update exercise 2 without second variable

--- src/tutorials/pynative/_1_basics/exercise2.py
+++ src/tutorials/pynative/_1_basics/exercise2.py
@@ -6,12 +6,11 @@ Print the sum of the current number and the previous number.
 
 def main() -> None:
     """Print current and previous number sum in a range."""
-    n = 10
+    limit = 10
     n1 = 0
-    n2 = 0
-    for _number in range(n):
+    for n2 in range(limit):
         print("Current Number %d Previous Number %d Sum: %d" % (n2, n1, n1 + n2))
-        n1, n2 = n2, n2 + 1
+        n1 = n2
 
 
 main()
