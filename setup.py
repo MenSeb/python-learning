34023a3aaae553516a0e95eec3de27d2e42a77ea
MenSeb <sebastien.menard.github@gmail.com>
2023-08-22 21:19:29 -0400

    chore(setup): remove setup file content

--- setup.py
+++ setup.py
@@ -1,11 +1,1 @@
 """Setup."""
-
-from setuptools import setup
-
-setup(
-    name="Dummy",
-    author="MenSeb",
-    version="1",
-    package_data={"dummy": ["py.typed"]},
-    packages=["dummy"],
-)
