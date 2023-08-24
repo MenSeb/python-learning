a01ea07c258b643513fe0bfd0419625554606e8d
MenSeb <sebastien.menard.github@gmail.com>
2023-08-24 14:28:35 -0400

    chore(record): updare record helpers with queries

--- src/mysql/record.py
+++ src/mysql/record.py
@@ -5,6 +5,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from logger import error_mysql, sucess_mysql
+from query import (
+    query_columns,
+    query_condition,
+    query_ignore,
+    query_limit,
+    query_order,
+    query_priority,
+)
 
 from mysql.connector import Error
 
@@ -29,14 +37,14 @@ def insert_record(
 
 def insert_records(
     table: str,
-    attrs: str,
-    vals: str,
+    columns: str,
+    values: str,
     records: list[tuple],
     cursor: MySQLCursor,
 ) -> None:
     """Insert multiple records in a MySQL table."""
     try:
-        cursor.executemany(f"INSERT INTO {table} {attrs} VALUES {vals}", records)
+        cursor.executemany(f"INSERT INTO {table} {columns} VALUES {values}", records)
     except Error as e:
         error_mysql(e, f"Failed to insert data in table {table}.")
     else:
@@ -45,15 +53,22 @@ def insert_records(
 
 def select_record(
     table: str,
-    condition: str | None,
     cursor: MySQLCursor,
+    **options: bool | str | None,
 ) -> None:
     """Read a record in a MySQL table."""
     try:
-        query = f"SELECT * FROM {table}"
-        if condition:
-            query += f" WHERE {condition}"
+        columns = query_columns(options.get("columns"))
+        condition = query_condition(options.get("condition"))
+        order = query_order(options.get("order"))
+        query = f"""
+            SELECT {columns}
+            FROM {table}
+            {condition}
+            {order}
+        """.rstrip()
         cursor.execute(query)
+        sucess_mysql(f"{query}\n")
     except Error as e:
         error_mysql(e, f"Failed to select data in table {table}.")
     else:
@@ -63,22 +78,45 @@ def select_record(
 def update_record(
     table: str,
     assignment: str,
-    condition: str,
     cursor: MySQLCursor,
+    **options: bool | str | None,
 ) -> None:
     """Update a record in a MySQL table."""
     try:
-        cursor.execute(f"UPDATE {table} SET {assignment} WHERE {condition}")
+        priority = query_priority(options.get("priority"))
+        ignore = query_ignore(options.get("ignore"))
+        condition = query_condition(options.get("condition"))
+        query = f"""
+            UPDATE {priority} {ignore} {table}
+            SET {assignment}
+            {condition}
+        """.rstrip()
+        cursor.execute(query)
+        sucess_mysql(f"{query}\n")
     except Error as e:
         error_mysql(e, f"Failed to update data in table {table}.")
     else:
         sucess_mysql(f"Data in table {table} updated successfully.")
 
 
-def delete_record(table: str, condition: str, cursor: MySQLCursor) -> None:
+def delete_record(
+    table: str,
+    cursor: MySQLCursor,
+    **options: bool | str | None,
+) -> None:
     """Delete a record in a MySQL table."""
     try:
-        cursor.execute(f"DELETE FROM {table} WHERE {condition}")
+        condition = query_condition(options.get("condition"))
+        limit = query_limit(options.get("limit"))
+        order = query_order(options.get("order"))
+        query = f"""
+            DELETE FROM {table}
+            {condition}
+            {order}
+            {limit}
+        """.rstrip()
+        cursor.execute(query)
+        sucess_mysql(f"{query}\n")
     except Error as e:
         error_mysql(e, f"Failed to delete data in table {table}.")
     else:
