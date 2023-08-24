679293fe2fc842f24e2417386512354cd482f170
MenSeb <sebastien.menard.github@gmail.com>
2023-08-24 13:19:59 -0400

    chore(logger): create logger for error and success messages

--- src/mysql/logger.py
+++ src/mysql/logger.py
@@ -2,13 +2,21 @@
 
 from __future__ import annotations
 
-from logging import error
+from logging import INFO, basicConfig, error, info
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from mysql.connector import Error
 
 
+basicConfig(level=INFO)
+
+
 def error_mysql(e: Error | AttributeError, msg: str) -> None:
-    """Raise error with custom message."""
+    """Output MySQL error with custom message."""
     error(f"{msg}\nTYPE: {type(e)}\nERROR: {e}")
+
+
+def sucess_mysql(message: str) -> None:
+    """Output MySQL success with custom message."""
+    info(message)
