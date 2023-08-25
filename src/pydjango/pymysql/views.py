43c11a89069e799fe964f5d178b168f86a554c01
MenSeb <sebastien.menard.github@gmail.com>
2023-08-25 17:42:58 -0400

    build(django): test views with urls

--- src/pydjango/pymysql/views.py
+++ src/pydjango/pymysql/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 
 
 # Create your views here.
 def index(request):
     """views index"""
     return HttpResponse("Hello Django!")
+
+
+def employee(request, id):
+    emp = Employee.objects().get(id=id)
+    return HttpResponse(f"Employee {id}: {emp}")
