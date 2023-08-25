27a130722c9985ed681faf6783cd12d913a9ca0f
MenSeb <sebastien.menard.github@gmail.com>
2023-08-25 16:26:53 -0400

    build(django): create and migrate models

--- src/pydjango/pymysql/models.py
+++ src/pydjango/pymysql/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Employee(models.Model):
+    """Model Employee."""
+
+    first_name = models.CharField(max_length=48)
+    last_name = models.CharField(max_length=48)
+    email = models.CharField(max_length=319)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self: "Employee") -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    """Model Task."""
+
+    employee = models.ForeignKey(Employee, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+
+    def __str__(self: "Task") -> str:
+        return f"{self.title}"
+
+    def is_assign(self: "Task") -> bool:
+        """Check if an employee is assigned to the task."""
+        return self.employee is not None
